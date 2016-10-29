0e441276f9bfa55a2b025af0e6540984bf562708
dean <Dean430@yeah.net>
2016-10-29 19:28:55 +0800

    即将开始模板引擎部分的编写工作

--- render.go
+++ render.go
@@ -1,7 +1,14 @@
 //模板设置
 package illusion
 
+//我以为Render只需要两个接口
 type Render interface {
-	//只要求是这一个接口就好了
-	render(string, ...interface{})
+	//渲染文件
+	view(string, ...interface{})
+
+	//这个起个什么名字好呢
+	//这个名字好像很不错
+	echo(...interface{})
 }
+
+
